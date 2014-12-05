57bbbc93232e470261e168e7145944b09853b8b3
Daniel Swann <dswann5@jhu.edu>
2014-12-05 20:47:23 +0000

    Added comments for PQueue.py

--- PQueue.py
+++ PQueue.py
@@ -2,6 +2,13 @@
 
 import pickle, random, Queue
 
+''' Object to hold metadata about individual proteins
+    Protein - string containing a potential protein for the sequence read
+    Poss - protein we hope to match, taken from the protein database
+    Curr_prot - tuple containing an index range of hits of the observed protein
+    Curr_poss - tuple containing an index range of hits of the ideal database protein
+    Score - initial heuristic value for this protein
+'''
 class ProteinIdentity:
     def __init__(self, protein, poss, curr_prot, curr_poss, score):
         self.protein = protein
@@ -39,18 +46,21 @@ class ProteinIdentity:
                 self.curr_prot[1] += 1
             if self.curr_poss[1] < len(self.poss) - 1:
                 self.curr_poss[1] += 1
-            
+    
+    """ We've reached the left end of the potential or observed protein """       
     def __at_left_end(self):
         if self.curr_prot[0] == 0 and self.curr_poss[0] == 0:
             self.left_end = True
 
+    """ We've reached the right end of the potential or observed protein """
     def __at_right_end(self):
         if self.curr_prot[1] == len(self.protein) - 1 and self.curr_poss[1] == len(self.poss) - 1:
-            self.right_end = True
+            self.right_end = True 
 
-    def __substitution(self, first, second):
-        
-
+''' Holds the logic for placing and retrieving proteins from the priority queue
+    read_proteins - list of possible proteins in the read
+    scheme - FreqScore object
+'''
 class PQueue:
  
     pqueue = Queue.PriorityQueue()
@@ -72,4 +82,4 @@ class PQueue:
                 self.blossum[key+columns[i]] = int(values[i])
    
     def __substitution(self, first, second):
-        return -self.blossum[first+second] #make it negative as lower values in PQueue are first
+        return -self.blossum[first+second] #flip sign as lower values in PQueue are first
