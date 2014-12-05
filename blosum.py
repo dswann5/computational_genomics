be5a78d598422ac70537a63b26cf4605c6b0dbd8
Sean <JoeSachmo@gmail.com>
2014-12-05 19:06:34 +0000

    add blosum matrix

---
+++ blosum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import numpy, sys
+        
+def main(argv):
+    blossum = {}
+    with open("blosum.txt", 'r') as f:
+            lines = f.read().splitlines()
+    columns = lines.pop(0).split()
+    for line in lines:
+        values = line.split()
+        key = values.pop(0)
+        for i in range(len(columns)):
+            blossum[key+columns[i]] = int(values[i])
+        
+    print blossum
+    
+if __name__ == '__main__':
+    main(sys.argv)
